b75626a6fa903f7cba69d00697e4794993454e52
Vishnu Das <dvishnudas@gmail.com>
2026-08-01 23:38:40 +0530

    feat : pwa added

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,4 +1,4 @@
-//import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 
 type userData = {
     name:string,
@@ -7,24 +7,25 @@ type userData = {
 
 const saveUserData  = async (data:userData) =>{
 
-    // Cookies.set("user_data", encodeURIComponent(JSON.stringify(data)), {
-    //     expires: 7,      // days
-    //     path: "/",       // accessible everywhere
-    // });
-    sessionStorage.setItem("user_data", JSON.stringify(data));
+    Cookies.set("user_data", encodeURIComponent(JSON.stringify(data)), {
+        expires: 25,      // days
+        path: "/",       // accessible everywhere
+    });
+    //sessionStorage.setItem("user_data", JSON.stringify(data));
     // console.log("Cookie Saved");
     return true;
 };
 
 const getUserData = async () =>{
-    //const userData = Cookies.get("user_data");
-    const userData = sessionStorage.getItem("user_data");
-    return userData?JSON.parse(userData):false;
+    const userData = Cookies.get("user_data");
+    // const userData = sessionStorage.getItem("user_data");
+    //return userData?JSON.parse(userData):false;
+    return userData?JSON.parse(decodeURIComponent(userData)):false;
 }
 
 const deleteUserData = () =>{
-    //Cookies.remove("user_data");
-    sessionStorage.removeItem("user_data");
+    Cookies.remove("user_data");
+    // sessionStorage.removeItem("user_data");
     return true;
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
             react(),
-            tailwindcss()
+            tailwindcss(),
+
+            //PWA Configuration
+            VitePWA({
+                registerType: 'autoUpdate',
+
+                manifest: {
+                  name: 'Vtrack',
+                  short_name: 'Vtrack',
+                  description: 'Personal Expense Assistant',
+                  theme_color: '#0d1524',
+                  background_color: '#0d1524',
+                  display: 'standalone',
+                  start_url: '/vtrack/',
+                  scope: '/vtrack/',
+                  icons: [
+                    {
+                      src: '/vtrack/assets/images/pwa/pwa-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png',
+                    },
+                    {
+                      src: '/vtrack/assets/images/pwa/pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                    },
+                    {
+                      src: '/vtrack/assets/images/pwa/pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any',
+                    },
+                    {
+                      src: '/vtrack/assets/images/pwa/pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'maskable',
+                    }
+                  ],
+                },
+              }),
+            //End PWA Configuration
           ],
   base:"/vtrack/"
 })
